a133e6dd15180677ab6e464968e89fd7eb912c61
Bonchang <9bonbon@naver.com>
2023-09-27 13:44:30 +0900

    change

--- SocketServer/src/SMain.java
+++ SocketServer/src/SMain.java
@@ -4,26 +4,32 @@ import java.net.*;
 public class SMain {
     public static void main(String[] args) {
         try {
-            // 서버에 연결
-            Socket socket = new Socket("localhost", 12345);
+            // 서버 소켓 생성 및 포트 번호 12345로 바인딩
+            ServerSocket serverSocket = new ServerSocket(12345);
+            System.out.println("서버가 시작되었습니다. 클라이언트의 연결을 기다립니다...");
 
-            // 서버로 데이터를 보내기 위한 출력 스트림 생성
-            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            // 클라이언트의 연결을 기다림
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("클라이언트가 연결되었습니다.");
 
-            // 서버로 메시지 보내기
-            out.println("Hello, Server!");
+            // 클라이언트로부터 데이터를 읽기 위한 입력 스트림 생성
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-            // 서버로부터 데이터를 읽기 위한 입력 스트림 생성
-            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            // 클라이언트로 데이터를 보내기 위한 출력 스트림 생성
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
 
-            // 서버로부터 응답 받아 화면에 출력
-            String response = in.readLine();
-            System.out.println("서버로부터 받은 응답: " + response);
+            // 클라이언트로부터 메시지를 읽고 화면에 출력
+            String message = in.readLine();
+            System.out.println("클라이언트로부터 수신된 메시지: " + message);
+
+            // 클라이언트에게 응답 보내기
+            out.println("Hello, Client!");
 
             // 자원 정리
             in.close();
             out.close();
-            socket.close();
+            clientSocket.close();
+            serverSocket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
